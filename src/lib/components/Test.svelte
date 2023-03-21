ca9ad38b5d6b20395809d6339059f2461700cef0
jaydenaguiar <jaydenaguiar@oldrochster.org>
2023-03-21 12:37:08 -0400

    created

---
+++ src/lib/components/Test.svelte
@@ -0,0 +1,4 @@
+
+<h1>
+	This is my test page.
+</h1>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,16 @@
+
+<script>
+	import Test from'$lib/components/test.svelte'
+</script>
+
+
+<Test/>
+<Test/>
+<Test/>
+<Test/>
+<Test/>
+<Test/>
+
+
+
+<h1> this is a test </h1>
