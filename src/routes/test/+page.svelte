c4598ca692347598e7d74c2b8889775b9b851ce0
jaydenaguiar <jaydenaguiar@oldrochster.org>
2023-04-13 11:49:26 -0400

    initial

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,16 +1,2 @@
+this is the test file
 
-<script>
-	import Test from'$lib/components/test.svelte'
-</script>
-
-
-<Test/>
-<Test/>
-<Test/>
-<Test/>
-<Test/>
-<Test/>
-
-
-
-<h1> this is a test </h1>
