cd7d728bfecc6d7a2aed1d54c46804f04791ce15
jaydenaguiar <jaydenaguiar@oldrochster.org>
2023-04-13 11:49:42 -0400

    initial

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,22 @@
-<!--This is your header-->
-<header>
-	This is a header!
+<script>
+	import Logo from '$lib/assets/portfolio-logo.svg';
+</script>
+
+<header
+	class="bg-indigo-100 text-white flex place-content-center min-w-full sm:px-5">
+	<div class="container flex items-center justify-between py-4">
+		<a href="/">
+			<! https://www.canva.com/design/DAFf0dO3D24/K4u_tkTZ2oEpgO7eMvt7Iw/edit?utm_content=DAFf0dO3D24&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton>
+			<img src="{Logo}" alt="Logo" class="max-h-12" />
+		</a>
+		<nav>
+			<ul class="flex items-center justify-center">
+				<li><a href="/" class="px-4 py-2 hover:text-gray-400">Home</a></li>
+				<li><a href="/form" class="px-4 py-2 hover:text-gray-400">Form</a></li>
+				<li><a href="/tribute" class="px-4 py-2 hover:text-gray-400">Tribute
+				</a></li>
+				<li><a href="/product" class="px-4 py-2 hover:text-gray-400">Product</a></li>
+			</ul>
+		</nav>
+	</div>
 </header>
